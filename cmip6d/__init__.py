45ec248f6eb179285aae4e129666dc7dfcf05efd
jonathanqv <jonathanqv1@gmail.com>
2022-11-08 11:08:48 -0700

    Creating package

---
+++ cmip6d/__init__.py
@@ -0,0 +1,1 @@
+from .cmip6d import cmip6d
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["beautifulsoup4"]
+
+setup(
+    name="cmip6d",
+    version="0.0.1",
+    author="Jonathan Quiroz",
+    author_email="jonathanqv1@gmail.com",
+    description="A package to download NASA NEX/GDDP-CMIP6 downscaled climate change scenarios",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonathanqv/cmip6d",
+    packages=find_packages(include=['cmip6d']),
+    install_requires=requirements,
+    license="MIT license",
+    license_files = ('LICENSE.txt'),
+)
